6b47332f86d4093623ed53e56f548d2a759b5e06
Alexey <alexeyinshakov992@gmail.com>
2025-04-11 14:20:06 +0300

    feat: 6 passing

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -121,8 +121,14 @@ function getPolynom(...args) {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  let cash;
+  return () => {
+    if (!cash) {
+      cash = func();
+    }
+    return cash;
+  };
 }
 
 /**
